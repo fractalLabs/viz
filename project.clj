af69210906ba1cd6500171f87ec68bc4b6b4594f
nez <andres@fractalmedia.mx>
2013-04-22 13:27:55 -0500

    Update project.clj
    compojure es dev dep

--- project.clj
+++ project.clj
@@ -2,10 +2,9 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
-  		 [hiccup "1.0.2"]
-                 [compojure "1.1.5"]]
+  		          [hiccup "1.0.2"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler viz.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.3"]
-  			[hiccup "1.0.2"]]}})
+  			                [compojure "1.1.5"]]}})
